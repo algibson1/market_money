246911401c5435f9e4ceb9b28ce65598835bf8a2
Artemy Gibson <artemygibson@gmail.com>
2023-08-28 18:14:04 -0700

    Add method to format only one market

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -17,4 +17,21 @@ class MarketSerializer
       }}
     end
   end
+
+  def self.format_market(market)
+    { 
+      id: market.id,
+      type: "market",
+      attributes: {
+        name: market.name,
+        street: market.street,
+        city: market.city,
+        county: market.county,
+        state: market.state,
+        zip: market.zip,
+        lat: market.lat,
+        lon: market.lon,
+        vendor_count: market.vendor_count
+    }}
+  end
 end
