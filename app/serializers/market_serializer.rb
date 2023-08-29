01ba012d54340495e3cacad988c9f0ba53943354
Artemy Gibson <artemygibson@gmail.com>
2023-08-28 18:06:28 -0700

    Fix formatting: make attributes a nested hash

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -3,16 +3,18 @@ class MarketSerializer
     markets.map do |market|
       { 
         id: market.id,
-        name: market.name,
-        street: market.street,
-        city: market.city,
-        county: market.county,
-        state: market.state,
-        zip: market.zip,
-        lat: market.lat,
-        lon: market.lon,
-        vendor_count: market.vendor_count
-      }
+        type: "market",
+        attributes: {
+          name: market.name,
+          street: market.street,
+          city: market.city,
+          county: market.county,
+          state: market.state,
+          zip: market.zip,
+          lat: market.lat,
+          lon: market.lon,
+          vendor_count: market.vendor_count
+      }}
     end
   end
 end
