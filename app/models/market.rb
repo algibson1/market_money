02426b544c7597f6428450e83871cf68139bcbe5
Artemy Gibson <artemygibson@gmail.com>
2023-08-28 17:03:19 -0700

    Add relationships and validations

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,3 +1,16 @@
 class Market < ApplicationRecord 
-  
+  has_many :market_vendors
+  has_many :vendors, through: :market_vendors
+  validates_presence_of :name,
+                        :street,
+                        :city,
+                        :county,
+                        :state,
+                        :zip,
+                        :lat,
+                        :lon
+
+  def vendor_count
+
+  end
 end
