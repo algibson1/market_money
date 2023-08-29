c19a577099b64baab2711428a91cd7e345a8bb93
Artemy Gibson <artemygibson@gmail.com>
2023-08-28 18:13:51 -0700

    Add test for singular market formatting method

--- spec/serializers/market_serializer_spec.rb
+++ spec/serializers/market_serializer_spec.rb
@@ -44,4 +44,45 @@ RSpec.describe MarketSerializer do
       expect(market[:attributes][:vendor_count]).to be_an(Integer)
     end
   end
+
+  it "can also format just one market" do
+    unformatted_market = create(:market)
+    market = MarketSerializer.format_market(unformatted_market)
+
+    expect(market).to have_key(:id)
+    expect(market[:id]).to eq(unformatted_market.id)
+
+    expect(market).to have_key(:type)
+    expect(market[:type]).to eq("market")
+
+    expect(market).to have_key(:attributes)
+    expect(market[:attributes]).to be_a(Hash)
+
+    expect(market[:attributes]).to have_key(:name)
+    expect(market[:attributes][:name]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:street)
+    expect(market[:attributes][:street]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:city)
+    expect(market[:attributes][:city]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:county)
+    expect(market[:attributes][:county]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:state)
+    expect(market[:attributes][:state]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:zip)
+    expect(market[:attributes][:zip]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:lat)
+    expect(market[:attributes][:lat]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:lon)
+    expect(market[:attributes][:lon]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:vendor_count)
+    expect(market[:attributes][:vendor_count]).to be_an(Integer)
+  end
 end
