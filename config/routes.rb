a293f60a3d2b88936e3ca9d183235325551614a5
Artemy Gibson <artemygibson@gmail.com>
2023-08-29 16:46:08 -0700

    Add route to market_vendors

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v0 do
-      resources :markets, only: [:index, :show]
+      resources :markets, only: [:index, :show] do
+        resources :vendors, only: [:index], controller: :market_vendors
+      end
     end
   end
 end
