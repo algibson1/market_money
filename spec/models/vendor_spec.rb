f35c3f8952e49c68f19598cb435a728f0168ed37
Artemy Gibson <artemygibson@gmail.com>
2023-08-28 17:02:36 -0700

    Create vendor model tests

---
+++ spec/models/vendor_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper" 
+
+RSpec.describe Vendor do
+  describe "relationships" do
+    it { should have_many :market_vendors }
+    it { should have_many :markets }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :contact_name }
+    it { should validate_presence_of :contact_phone }
+    it { should allow_value(true).for(:credit_accepted) }
+    it { should allow_value(false).for(:credit_accepted) }
+  end
+end
