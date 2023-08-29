a7a27763b15d60ad7ec1ec0a1602ab46b5a57d69
Artemy Gibson <artemygibson@gmail.com>
2023-08-29 11:54:13 -0700

    Add error handling to show method

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -4,6 +4,12 @@ class Api::V0::MarketsController < ApplicationController
   end
 
   def show
-    render json: MarketSerializer.new(Market.find(params[:id]))
+    begin
+      market = Market.find(params[:id])
+      render json: MarketSerializer.new(market)
+    rescue ActiveRecord::RecordNotFound => e
+      # render json: ErrorMarketSerializer.format_error(error)
+      render json: { errors: [{detail: e.message}] }, status: :not_found
+    end
   end
 end
