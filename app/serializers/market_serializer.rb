6d881292945bb6944a2e221f5839fa7005e464ea
Artemy Gibson <artemygibson@gmail.com>
2023-08-28 18:44:50 -0700

    Utilize JSONAPI gem to replace formatting methods

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,37 +1,49 @@
 class MarketSerializer 
-  def self.format_markets(markets)
-    markets.map do |market|
-      { 
-        id: market.id,
-        type: "market",
-        attributes: {
-          name: market.name,
-          street: market.street,
-          city: market.city,
-          county: market.county,
-          state: market.state,
-          zip: market.zip,
-          lat: market.lat,
-          lon: market.lon,
-          vendor_count: market.vendor_count
-      }}
-    end
-  end
+  include JSONAPI::Serializer
+  attributes  :name,
+              :street,
+              :city,
+              :county,
+              :state,
+              :zip,
+              :lat,
+              :lon,
+              :vendor_count
 
-  def self.format_market(market)
-    { 
-      id: market.id,
-      type: "market",
-      attributes: {
-        name: market.name,
-        street: market.street,
-        city: market.city,
-        county: market.county,
-        state: market.state,
-        zip: market.zip,
-        lat: market.lat,
-        lon: market.lon,
-        vendor_count: market.vendor_count
-    }}
-  end
+
+  # def self.format_markets(markets)
+  #   markets.map do |market|
+  #     { 
+  #       id: market.id,
+  #       type: "market",
+  #       attributes: {
+  #         name: market.name,
+  #         street: market.street,
+  #         city: market.city,
+  #         county: market.county,
+  #         state: market.state,
+  #         zip: market.zip,
+  #         lat: market.lat,
+  #         lon: market.lon,
+  #         vendor_count: market.vendor_count
+  #     }}
+  #   end
+  # end
+
+  # def self.format_market(market)
+  #   { 
+  #     id: market.id,
+  #     type: "market",
+  #     attributes: {
+  #       name: market.name,
+  #       street: market.street,
+  #       city: market.city,
+  #       county: market.county,
+  #       state: market.state,
+  #       zip: market.zip,
+  #       lat: market.lat,
+  #       lon: market.lon,
+  #       vendor_count: market.vendor_count
+  #   }}
+  # end
 end
