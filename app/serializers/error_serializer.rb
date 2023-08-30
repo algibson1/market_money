ea33f6ef57350b15f6911b1f5e528dfcfc5358d5
Artemy Gibson <artemygibson@gmail.com>
2023-08-29 18:47:37 -0700

    Add method to format invalid errors

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,10 +1,20 @@
 class ErrorSerializer
-  def self.format_error(error)
+  def self.not_found(error)
     { errors: [
         {
           detail: error.message
         }
       ]
     }
+  end
+
+
+  def self.invalid(errors)
+    { errors: [
+      {
+        detail: "Validation failed: #{errors.full_messages.join(", ")}"
+      }
+    ]
+  }
   end
 end
