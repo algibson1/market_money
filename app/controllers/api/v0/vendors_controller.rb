2590fb2f47e0f7e779402530d8c8ad1bcd807158
Artemy Gibson <artemygibson@gmail.com>
2023-08-29 19:28:58 -0700

    Add create method and vendor_params private method

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -4,7 +4,22 @@ class Api::V0::VendorsController < ApplicationController
       vendor = Vendor.find(params[:id])
       render json: VendorSerializer.new(vendor)
     rescue ActiveRecord::RecordNotFound => error
-      render json: ErrorSerializer.format_error(error), status: :not_found
+      render json: ErrorSerializer.not_found(error), status: :not_found
     end
+  end
+
+  def create
+    vendor = Vendor.new(vendor_params)
+    if vendor.save
+      render json: VendorSerializer.new(vendor), status: :created
+    else 
+      render json: ErrorSerializer.invalid(vendor.errors), status: :bad_request
+    end
+  end
+
+  private
+
+  def vendor_params
+    params.permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
   end
 end
