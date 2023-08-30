2edfc7a86c63e05d2230ebb2b522b3d2e453a38b
Artemy Gibson <artemygibson@gmail.com>
2023-08-29 20:20:09 -0700

    Add update and find_vendor action

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,11 +1,8 @@
 class Api::V0::VendorsController < ApplicationController
+  before_action :find_vendor, only: [:show, :update]
+
   def show
-    begin
-      vendor = Vendor.find(params[:id])
-      render json: VendorSerializer.new(vendor)
-    rescue ActiveRecord::RecordNotFound => error
-      render json: ErrorSerializer.not_found(error), status: :not_found
-    end
+    render json: VendorSerializer.new(@vendor)
   end
 
   def create
@@ -17,7 +14,24 @@ class Api::V0::VendorsController < ApplicationController
     end
   end
 
+  def update
+    @vendor.update(vendor_params)
+    if @vendor.save
+      render json: VendorSerializer.new(@vendor)
+    else 
+      render json: ErrorSerializer.invalid(@vendor.errors), status: :bad_request
+    end
+  end
+
   private
+
+  def find_vendor
+    begin
+      @vendor = Vendor.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => error
+      render json: ErrorSerializer.not_found(error), status: :not_found
+    end
+  end
 
   def vendor_params
     params.permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
