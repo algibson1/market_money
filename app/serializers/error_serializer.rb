ee760048e20282b7ec975d67acaa8fc73fb533c8
Artemy Gibson <artemygibson@gmail.com>
2023-08-30 16:14:48 -0700

    Add no_association method to format market_vendor errors

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -17,4 +17,14 @@ class ErrorSerializer
     ]
   }
   end
+
+  def self.no_association(ids)
+    {
+      errors: [
+          {
+              detail: "No association exists between market with 'id'=#{ids["market_id"] || "N/A"} AND vendor with 'id'=#{ids["vendor_id"] || "N/A"}"
+          }
+      ]
+  }
+  end
 end
