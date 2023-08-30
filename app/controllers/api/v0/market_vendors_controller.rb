3bb3b0f9bdf9fd026dde8c9ea79e03a5a20ff3f1
Artemy Gibson <artemygibson@gmail.com>
2023-08-30 14:38:21 -0700

    Add create method and private strong params method

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -8,4 +8,21 @@ class Api::V0::MarketVendorsController < ApplicationController
     end
   end
 
+  def create
+    market_vendor = MarketVendor.new(market_vendor_params)
+      if market_vendor.save 
+        render json: {message: "Successfully added vendor to market"}, status: :created
+      elsif params[:market_id].nil? || params[:vendor_id].nil?
+        render json: ErrorSerializer.invalid(market_vendor.errors), status: :bad_request
+      elsif MarketVendor.find_by(market_vendor_params)
+        render json: ErrorSerializer.invalid(market_vendor.errors), status: :unprocessable_entity
+      else
+        render json: ErrorSerializer.invalid(market_vendor.errors), status: :not_found
+      end
+  end
+
+  private
+  def market_vendor_params
+    params.permit(:market_id, :vendor_id)
+  end
 end
