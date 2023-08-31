192a8d8bb1a71138dc6a526cb1462bab993a370e
Artemy Gibson <artemygibson@gmail.com>
2023-08-30 21:20:06 -0700

    Add search action and search_params private action

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -11,4 +11,18 @@ class Api::V0::MarketsController < ApplicationController
       render json: ErrorSerializer.not_found(error), status: :not_found
     end
   end
+
+  def search
+    search = MarketSearch.new(search_params)
+    if search.valid?
+      render json: MarketSerializer.new(Market.search(search))
+    else
+      render json: ErrorSerializer.invalid_search, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def search_params
+    params.permit(:state, :city, :name).to_hash
+  end
 end
