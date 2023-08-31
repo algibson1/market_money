ebda9f96219ee8f7d4fb2fcbe0f295b07f9a4621
Artemy Gibson <artemygibson@gmail.com>
2023-08-31 14:39:12 -0700

    Add nearest_atms method and find_market private method

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,15 +1,12 @@
 class Api::V0::MarketsController < ApplicationController
+  before_action :find_market, only: [:show, :nearest_atms]
+
   def index
     render json: MarketSerializer.new(Market.all)
   end
 
   def show
-    begin
-      market = Market.find(params[:id])
-      render json: MarketSerializer.new(market)
-    rescue ActiveRecord::RecordNotFound => error
-      render json: ErrorSerializer.not_found(error), status: :not_found
-    end
+    render json: MarketSerializer.new(@market)
   end
 
   def search
@@ -21,8 +18,20 @@ class Api::V0::MarketsController < ApplicationController
     end
   end
 
+  def nearest_atms
+    render json: AtmSerializer.new(AtmFacade.atms_for(@market))
+  end
+
   private
   def search_params
     params.permit(:state, :city, :name).to_hash
+  end
+
+  def find_market
+    begin
+      @market = Market.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => error
+      render json: ErrorSerializer.not_found(error), status: :not_found
+    end
   end
 end
