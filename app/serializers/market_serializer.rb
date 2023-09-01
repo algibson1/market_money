7415d1525590261c6ca81a64b6d5f9cd18f97245
Artemy Gibson <artemygibson@gmail.com>
2023-09-01 14:28:53 -0700

    Delete comments

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -10,40 +10,4 @@ class MarketSerializer
               :lon,
               :vendor_count
 
-
-  # def self.format_markets(markets)
-  #   markets.map do |market|
-  #     { 
-  #       id: market.id,
-  #       type: "market",
-  #       attributes: {
-  #         name: market.name,
-  #         street: market.street,
-  #         city: market.city,
-  #         county: market.county,
-  #         state: market.state,
-  #         zip: market.zip,
-  #         lat: market.lat,
-  #         lon: market.lon,
-  #         vendor_count: market.vendor_count
-  #     }}
-  #   end
-  # end
-
-  # def self.format_market(market)
-  #   { 
-  #     id: market.id,
-  #     type: "market",
-  #     attributes: {
-  #       name: market.name,
-  #       street: market.street,
-  #       city: market.city,
-  #       county: market.county,
-  #       state: market.state,
-  #       zip: market.zip,
-  #       lat: market.lat,
-  #       lon: market.lon,
-  #       vendor_count: market.vendor_count
-  #   }}
-  # end
 end
